18502aea9ee4eafdbba994279f85e74aac0325aa
Alastair Surin <asurin@weddingwire.com>
2016-12-20 13:14:14 -0500

    Update QuickMain.swift

--- Sources/Quick/QuickMain.swift
+++ Sources/Quick/QuickMain.swift
@@ -1,5 +1,4 @@
 import XCTest
-#if os(Linux)
 // NOTE: This file is not intended to be included in the Xcode project or CocoaPods.
 //       It is picked up by the Swift Package Manager during its build process.
 
